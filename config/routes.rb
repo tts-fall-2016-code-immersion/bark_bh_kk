ff2c051c13e40792ff50de40db6b4e97e3109be8
Katie <newkatiek@gmail.com>
2016-11-02 10:34:56 -0400

    changes made up until end of class last night

--- config/routes.rb
+++ config/routes.rb
@@ -1,15 +1,9 @@
 Rails.application.routes.draw do
-  get 'tweets/index'
+  resources :tweets
 
-  get 'tweets/new'
-
-  get 'tweets/edit'
-
-  get 'tweets/show'
-
-  get 'tweets/_form'
 
   devise_for :users
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
-root to: "home#index"
+
+root to: "tweets#index"
 end
